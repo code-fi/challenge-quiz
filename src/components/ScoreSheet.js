c426498f56462390e03f7a910780864832e5e7b0
Frank <codewithfrank@gmail.com>
2020-12-29 15:38:40 +0000

    patch: updated retake button text

--- src/components/ScoreSheet.js
+++ src/components/ScoreSheet.js
@@ -1,48 +1,48 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 const Sheet = styled.div`
   width: 730px;
   padding: 40px;
   display: flex;
   flex-direction: column;
-`;
+`
 
 const Line = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 4px 0;
-`;
+`
 
 const Mark = styled.span`
   font-size: 14px;
   font-weight: bold;
-  color: ${(props) => (props.isCorrect ? "green" : "red")};
-`;
+  color: ${(props) => (props.isCorrect ? 'green' : 'red')};
+`
 
-function ScoreSheet({ answers, onRestartTest }) {
+function ScoreSheet ({ answers, onRestartTest }) {
   const renderAnswers = answers.map(({ questionNumber, isCorrect }) => (
     <AnswerSheet
       questionNumber={questionNumber}
       isCorrect={isCorrect}
       key={answers.questionNumber}
     />
-  ));
+  ))
   return (
     <Sheet>
       {renderAnswers}
-      <button onClick={onRestartTest}>Restart</button>
+      <button onClick={onRestartTest}>Retake Test</button>
     </Sheet>
-  );
+  )
 }
 
 const AnswerSheet = ({ questionNumber, isCorrect }) => {
   return (
     <Line>
       <span>Question {questionNumber}</span>
-      <Mark isCorrect={isCorrect}>{isCorrect ? "correct" : "wrong"}</Mark>
+      <Mark isCorrect={isCorrect}>{isCorrect ? 'correct' : 'wrong'}</Mark>
     </Line>
-  );
-};
+  )
+}
 
-export default ScoreSheet;
+export default ScoreSheet
